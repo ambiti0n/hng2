6e892967f76775401bc28a69951104e8a92d3fec
ambiti0n <36278808+ambiti0n@users.noreply.github.com>
2021-08-18 01:26:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,4 +23,6 @@
  })
 
 
- app.listen(3000, () => console.log('Server started...'));
+ const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
